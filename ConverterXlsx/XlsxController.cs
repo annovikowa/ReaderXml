ff8b75e8adc107060673bf833e9ee39435a45758
Анна Новикова <annovikowa@gmail.com>
2020-06-18 20:33:15 +0300

    Добавление прокта aps.net

---
+++ ConverterXlsx/XlsxController.cs
@@ -0,0 +1,20 @@
+﻿using ClosedXML.Excel;
+using ReaderXml;
+using ReaderXml.ExelSheets;
+using System.Web.Http;
+
+namespace ConverterXlsx
+{
+    public class XlsxController : ApiController
+    {
+        public XLWorkbook Get(string path)
+        {
+            var reader = new CadastralPlanTerritoryReader();
+            var KPT = reader.Read(path);
+
+            ExelFiller exelFiller = new ExelFiller(KPT);
+            exelFiller.XlWorkbook.SaveAs("test.xlsx");
+            return exelFiller.XlWorkbook;
+        }
+    }
+}
